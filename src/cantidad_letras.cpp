a6a9d06ca8de7608c430ceea69a52046d7e8c93e
josemam <jmlosvillares@correo.ugr.es>
2016-01-13 21:22:39 +0100

    cantidad_letras

---
+++ src/cantidad_letras.cpp
@@ -0,0 +1,49 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include "diccionario.h"
+
+int main(int argc, char * argv[]){
+  if (argc != 4) {
+	 cerr << "Uso: " << argv[0] << " diccionario letras salida" << endl;
+	 return 1;
+  }
+
+  ifstream fd(argv[1]);
+  if (!fd) {
+    cerr << "No se pudo cargar el diccionario" << endl;
+    return 1;
+  }
+  
+  Diccionario D;
+  fd >> D;
+  
+  ifstream fl(argv[2]);
+  if (!fl) {
+    cerr << "No se pudo cargar el fichero con las letras" << endl;
+    return 1;
+  }
+  
+  Conjunto_Letras cl;
+  fl >> cl;
+  
+  Conjunto_Letras::iterator cli;
+  for (cli = cl.begin(); cli != cl.end(); ++cli)
+    (*cli).n = 0;
+  
+  for (Diccionario::iterator di = D.begin(); di != D.end(); ++di)
+    for (int k = 0; k < di.cad.length(); k++)
+      *(cl.Encuentra(di.cad[k])).n++;
+  }
+
+  ofstrem fo(argv[3]);
+  if (!fo) {
+     cerr << "No se pudo imprimir el resultado" << endl;
+     return 1;
+  }
+  fo << "#Letra\tCantidad\tPuntos\n";
+
+  for (cli = cl.begin(); cli != cl.end(); ++cli)
+    cout << '\n' << (*cli).l << '\t' << (*cli).n << '\t' << (*cli).puntos;
+
+}
